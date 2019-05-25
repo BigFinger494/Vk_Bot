a3e8132c85d483558efa079f5e16139a837fdebd
BigFinger494 <BigFinger494@gmail.com>
2019-05-26 00:03:36 +0300

    qwerty
    1 version

---
+++ Vk_Bot/Vk_Bot/Controllers/BotController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Vk_Bot.Controllers
+{
+    [Serializable]
+    public class BotController
+    {
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("object")]
+        public string Object { get; set; }
+
+        [JsonProperty("group_id")]
+        public string Group_Id { get; set; }
+    }
+}
---
+++ Vk_Bot/Vk_Bot/Controllers/CallBackController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using VkNet.Abstractions;
+using VkNet.Model.RequestParams;
+using VkNet.Model;
+using VkNet.Utils;
+
+namespace Vk_Bot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CallBackController: ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        private readonly IVkApi vkApi;
+        public CallBackController(IConfiguration config,IVkApi vkontApi)
+        {
+            configuration = config;
+            vkApi = vkontApi;
+        }
+        [HttpPost]
+        public IActionResult Callback([FromBody] BotController updates)
+        {
+            switch (updates.Type)
+            {
+                case "confirmation":
+                    return Ok(configuration["Config:Confirmation"]);
+                case "message_new":
+                    {
+                        var msg = Message.FromJson(new VkResponse(updates.Object));
+
+                        vkApi.Messages.Send(new MessagesSendParams
+                        {
+                            RandomId = new DateTime().Millisecond,
+                            PeerId = msg.PeerId.Value,
+                            Message = msg.Text
+                        });
+                        break;
+                    }
+                
+            }
+            return Ok("ok");
+        }
+    }
+}
